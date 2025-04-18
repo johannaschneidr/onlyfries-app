d7a91dc0b0bb4ab6d7e9deeba9e10482ef0c8dea
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-04-18 15:11:14 -0400

    Google Maps Places API added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../global.css'
+import Script from 'next/script'
 
+// Google Maps Integration
+// This script loads the Google Maps JavaScript API with Places library
+// The API key is stored in NEXT_PUBLIC_GOOGLE_MAPS_API_KEY environment variable
+// The 'beforeInteractive' strategy ensures the script loads before the page becomes interactive
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+        strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp 
