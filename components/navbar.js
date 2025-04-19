d04cf150955af3a594dee73da9a24c88a0700318
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-04-19 18:34:07 -0400

    UI improvements post cards

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,18 @@
 // components/Navbar.js
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="p-4 border-b flex justify-between items-center">
-      <Link href="/" className="text-xl font-bold">OnlyFries</Link>
-      <div className="flex gap-4">
-        <Link href="/" className="hover:text-yellow-500">Home</Link>
-      </div>
+    <nav className="p-4 flex justify-between items-center">
+      <Link href="/" className="relative w-[120px] h-[40px]">
+        <Image
+          src="/assets/logo.png"
+          alt="OnlyFries Logo"
+          fill
+          className="object-contain"
+        />
+      </Link>
     </nav>
   );
 }
