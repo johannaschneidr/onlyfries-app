2a37a82b4e4b45038d4203bb4121bf0e8d575dae
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-11-03 20:45:59 -0500

    Footer, Nav bar changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec4c7191bc06e5e5"; }
+/******/ 		__webpack_require__.h = function() { return "a2c30959f0822665"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
