1bc57c94c37e3b902a1fd482c4cb06171b184ead
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-11-03 22:12:17 -0500

    background for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af336fbd277ebb37"; }
+/******/ 		__webpack_require__.h = function() { return "5677f3dc463de110"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
